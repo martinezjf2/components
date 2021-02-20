8571059445bb9897e8c7385e98f9b06e24b47f5b
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-20 14:57:16 -0500

    created styling with semantic ui for the approval card

--- src/ApprovalCard.jsx
+++ src/ApprovalCard.jsx
@@ -2,8 +2,15 @@ import React from "react"
 
 function ApprovalCard() {
     return (
-        <div>
-            Hello
+        <div className="ui card">
+            <div className="content">
+            <div className="extra content">
+                <div className="ui two buttons">
+                    <div className="ui basic green button">Approve</div>
+                    <div className="ui basic red button">Reject</div>
+                </div>
+            </div>
+            </div>
         </div>
     )
 }
