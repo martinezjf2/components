793bcab8b0419ec7a8e8e1a82cf41b52c82e36d4
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-19 23:46:21 -0500

    created App component and moved code from the index to the App component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CommentDetail from './CommentDetail'
+
+export default function App() {
+    return (
+    <div className="ui container comments">
+        <CommentDetail />
+    </div>
+    )
+};
--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import CommentDetail from './CommentDetail'
-
-const App = () => {
-    return (
-    <div className="ui container comments">
-        <CommentDetail />
-    </div>
-    )
-};
+import App from './App'
 
 
 
