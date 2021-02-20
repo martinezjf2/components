0aaa7755239da0563410ca60bc03c39915375a8b
Jeffrey Martinez <martinezjf2@gmail.com>
2021-02-20 15:00:37 -0500

    imported approvalcard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import CommentDetail from './CommentDetail';
-import  data  from './data'
+import  data  from './data';
+import ApprovalCard from './ApprovalCard'
 
 export default function App() {
     return (
         
     <div className="ui container comments">
+        <ApprovalCard />
         {data.map((comment) => {
            return (
            <CommentDetail name={comment.name} date={comment.date}message={comment.message}/>
